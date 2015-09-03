61cf2b7d41aa24c124dd0349bf162664d8940edc
Wesley <wesley.werner@gmail.com>
2015-09-03 16:28:40 +0200

    Remove unused table printing function.

--- map.lua
+++ map.lua
@@ -8,31 +8,6 @@ require 'astar'
 
 SlimeMapHandler = class('SlimeMapHandler')
 
--- Print anything - including nested tables
-function table_print (tt, indent, done)
-  done = done or {}
-  indent = indent or 0
-  if type(tt) == "table" then
-    for key, value in pairs (tt) do
-      io.write(string.rep (" ", indent)) -- indent it
-      if type (value) == "table" and not done [value] then
-        done [value] = true
-        io.write(string.format("[%s] => table\n", tostring (key)));
-        io.write(string.rep (" ", indent+4)) -- indent it
-        io.write("(\n");
-        table_print (value, indent + 7, done)
-        io.write(string.rep (" ", indent+4)) -- indent it
-        io.write(")\n");
-      else
-        io.write(string.format("[%s] => %s\n",
-            tostring (key), tostring(value)))
-      end
-    end
-  else
-    io.write(tt .. "\n")
-  end
-end
-
 function SlimeMapHandler:convert(mask)
     -- Converts a walkable image mask into map points.
     local mask = mask:getData()
